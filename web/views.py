a08b82bc38c8879cbb655b917744dc0984d675b1
Nattanan Nawakitbamrung <6210612567@student.tu.ac.th>
2022-10-03 03:01:20 +0700

    Update index.html & login.html

--- web/views.py
+++ web/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from .authentication import ldap3_authen
 # Create your views here.
 
+
 def index(request):
     return render(request, "web/index.html")
 
@@ -16,11 +17,9 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        response = ldap3_authen(username,password)
+        response = ldap3_authen(username, password)
         if response[0] == True:
-            return render(request, "web/index.html",{'name':response[1]})
+            return render(request, "web/index.html", {'name': response[1]})
         else:
             return render(request, "web/login.html")
     return render(request, "web/login.html")
-    
-
